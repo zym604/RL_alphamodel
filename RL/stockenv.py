544ae9415d84d79522e97d79d2f3024c222e67df
zym604 <zym0326@hotmail.com>
2020-03-03 17:11:21 -0500

    getstate函数

--- RL/stockenv.py
+++ RL/stockenv.py
@@ -5,10 +5,19 @@ class stockenv:
         self.action_space = [0,10,20,30,40,50,60,70,80,90,100]
         self.action_number = len(self.action_space)
         self.histry_action = []
+        self.path = r'E:\Lean-master\Launcher\bin\Debug\QuantConnect.Lean.Launcher.exe'
     def reset(self):
         self.histry_action = []
+    def getexeoutput(self):
+        import subprocess
+        p1 = subprocess.Popen(self.path,cwd=r'E:\Lean-master\Launcher\bin\Debug',stdin=subprocess.PIPE,stdout=subprocess.PIPE, shell=True)
+        self.output = p1.communicate()[0]
+        return self.output
     def getstate(self):
-        #返回state，也就是多个不同alpha model的insights
+        #返回state，也就是多个不同alpha model的insights,最好不要读写文件
+        self.getexeoutput()
+        state =  self.output.splitlines()
+        return state
     def step(self,action):
         self.action = action
         #change file parameters: action, start time, etc
@@ -20,14 +29,20 @@ class stockenv:
             done = False
         else:
             done = True
-        return reward, done
+        # state = 让quantconnect返回
+        state = 0
+        return state,reward, done
     def close(self):
         self.histry_action = []
 
 env = stockenv()
 env.reset()
-for action in [60,20,70,40,10]:
-    reward, done = env.step(action)
-    print(action, done)
-print(env.action_space)
-print(env.action_number)
+state = env.getstate()
+for s in state:
+    print(s)
+
+# for action in [60,20,70,40,10]:
+#     state, reward, done = env.step(action)
+#     print(action, done)
+# print(env.action_space)
+# print(env.action_number)
--- interact_lLean/2getexeresult.py
+++ interact_lLean/2getexeresult.py
@@ -1,8 +1,5 @@
-from subprocess import check_output as qx
-
-#cmd = r'E:\Lean-master\Launcher\bin\Debug\QuantConnect.Lean.Launcher.exe'
-#output = qx(cmd)
-#print(output)
-#用不起来，还是让lean每次自己生成一个result.txt小文件，然后读取吧
-a = qx(['ls', '-l'])
-print(a)
+import subprocess
+path = r'E:\Lean-master\Launcher\bin\Debug\QuantConnect.Lean.Launcher.exe'
+p1 = subprocess.Popen(path,cwd=r'E:\Lean-master\Launcher\bin\Debug',stdin=subprocess.PIPE,stdout=subprocess.PIPE, shell=True)
+state = p1.communicate()[0]
+print(state)
